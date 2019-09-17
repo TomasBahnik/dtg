4be4b31d11c9cccadbd5e43ee783eb2f304e02d6
Tomas Bahnik <tomas.bahnik@email.cz>
2019-09-17 17:41:02 +0000

    insert to calendar

--- vyplatyNG/kalendar.pas
+++ vyplatyNG/kalendar.pas
@@ -15,6 +15,7 @@ type
     Memo1: TMemo;
     toDateTimePicker: TDateTimePicker;
     fromDateTimePicker: TDateTimePicker;
+    procedure AddClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,5 +30,31 @@ implementation
 uses dtgDM;
 
 {$R *.DFM}
+
+procedure TForm1.AddClick(Sender: TObject);
+var
+  fromDate,toDate,aDate : TDate;
+  numOfDays,i     : Integer;
+begin
+   toDate := toDateTimePicker.Date;
+   fromDate := fromDateTimePicker.Date;
+   numOfDays := Trunc(toDate - fromDate);
+   //fromDay := LongDayNames[DayOfWeek(fromDate)];
+   //toDay := LongDayNames[DayOfWeek(toDate)];
+   //ShowMessage(DateToStr(toDate)+ ' is ' + toDay + ' diff is ' + IntToStr(period));
+   for i := 0 to numOfDays do
+      begin
+         with dtgDataModule.kalendarTbl do
+           begin
+              aDate := fromDate + i;
+              Insert;
+              FieldByName('Datum').AsDateTime := aDate;
+              FieldByName('Den').AsInteger := DayOfWeek(aDate);
+              FieldByName('DruhDne').AsInteger := DayOfWeek(aDate) + 1;
+              FieldByName('Tyden').AsInteger := DayOfWeek(aDate) + 2;
+              Post;
+           end;
+      end;
+end;
 
 end.
