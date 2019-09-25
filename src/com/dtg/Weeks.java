2d5f370a00f1ff2a74339a8e5cff52eddadab119
Tomas Bahnik <tbahnik@casenetllc.com>
2019-09-25 08:19:22 +0200

    isNewYearAfterThu

--- src/com/dtg/Weeks.java
+++ src/com/dtg/Weeks.java
@@ -8,10 +8,11 @@ public class Weeks {
 
     private static final int STD_NUM_OF_WEEKS = 52;
     private int compareWithYear = 0;
+    private boolean isNewYearAfterThu = false;
     private int week = 1;
     private int startWeek = 1;
     private static LocalDate startDate = LocalDate.of(2020, 1, 1);
-    private static LocalDate endDate = LocalDate.of(2022, 1, 4);
+    private static LocalDate endDate = LocalDate.of(2023, 1, 30);
 
 
     private int getWeekOfDay(LocalDate day) {
@@ -20,13 +21,19 @@ public class Weeks {
         if (actualYear == compareWithYear) {
             if (dayOfWeek == DayOfWeek.MONDAY) {
                 System.out.println(" --------------------------------------- ");
-                week = week >= STD_NUM_OF_WEEKS ? 1 : week + 1;
+                if (week == STD_NUM_OF_WEEKS && isNewYearAfterThu) {
+                    week = 1;
+                    return week;
+                }
+                week = week > STD_NUM_OF_WEEKS ? 1 : week + 1;
                 return week;
             } else
                 return week;
         } else {
             //new year
             compareWithYear = day.getYear();
+            LocalDate newYear = LocalDate.of(compareWithYear, 1, 1);
+            isNewYearAfterThu = afterThu(newYear);
             if (!afterThu(day) || dayOfWeek == DayOfWeek.MONDAY) {
                 System.out.println(" *** new year **** first week *****");
                 week = 1;
@@ -44,6 +51,8 @@ public class Weeks {
     public static void main(String[] args) {
         Weeks weeks = new Weeks();
         weeks.compareWithYear = startDate.getYear();
+        LocalDate newYear = LocalDate.of(weeks.compareWithYear, 1, 1);
+        weeks.isNewYearAfterThu = weeks.afterThu(newYear);
         weeks.week = weeks.startWeek;
         long days = ChronoUnit.DAYS.between(startDate, endDate);
         //int period = Period.between(startDate, endDate).
